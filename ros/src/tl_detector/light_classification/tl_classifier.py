d2446740e692b0a3aa5c72a8538e476eff8dd50d
Nalini Sharma <nalini.sharma@gmail.com>
2017-12-13 06:49:37 -0800

    Added some classifier code

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,58 +1,82 @@
 from styx_msgs.msg import TrafficLight
 
-# adding code assuming that we have a model that classifies the lights
 
-
-import cv2
 import numpy as np
+import os
+import sys
 import tensorflow as tf
-from keras import backend as K
-from keras import layers
-from keras.models import load_model
+
+# object detection imports
+from object_detection.utils import label_map_util
 
 class TLClassifier(object):
     def __init__(self):
-
         #TODO load classifier
 
-        config = tf.ConfigProto()
-        config.gpu_options.allow_growth = True
-        sess = tf.Session(config=config)
-        K.set_session(sess)
+        # add code to check if this is the real world or simulator and then call sim_model or real_model
+ 
 
-        # load model here 
-        # self.model =
+    def sim_model(image):
+    
+        # code for the simulator model     
+        # sim_model = 'add path to the model'
+        detection_graph = tf.Graph()
 
-        #TODO as soon as we load the model add this again
-        #self.get_output = K.function([self.model.layers[0].input, K.learning_phase()],
-        #                             [self.model.layers[-1].output])
+        with detection_graph.as_default():
+    
+                od_graph_def = tf.GraphDef()
+
+                with tf.gfile.GFile(sim_model, 'rb') as fid:
+        
+                        serialized_graph = fid.read()
+                        od_graph_def.ParseFromString(serialized_graph)
+                        tf.import_graph_def(od_graph_def, name='')
+        
+
+        
+                with tf.Session(graph=detection_graph) as sess:
+                        # Definite input and output Tensors for detection_graph
+                        image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+        
+                        # Each box represents a part of the image where a particular object was detected.
+                        detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+        
+                        # Each score represent level of confidence for each of the objects.
+                        # Score is shown on the result image, together with the class label.
+                        detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
+                        detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
+                        num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+            
+                        (im_width, im_height) = image.size
+                        image_np = np.array(image.getdata()).reshape((im_height, im_width, 3)).astype(np.uint8)
+                        # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+                        image_np_expanded = np.expand_dims(image_np, axis=0)
 
 
-       # pass
+                        # Actual detection.
+                        (boxes, scores, classes, num) = sess.run(
+                                [detection_boxes, detection_scores, detection_classes, num_detections],
+                                feed_dict={image_tensor: image_np_expanded})
+
+                        boxes = np.squeeze(boxes)
+                        scores = np.squeeze(scores)
+                        classes = np.squeeze(classes).astype(np.int32)
+
+                        min_score_thresh = .50
+                        for i in range(boxes.shape[0]):
+                                if scores is None or scores[i] > min_score_thresh:
+
+                                        # use class and score to detect the red light
+
 
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
-
         Args:
             image (cv::Mat): image containing the traffic light
-
         Returns:
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
-
         """
         #TODO implement light color prediction
 
-
-        image = cv2.resize(image, (224, 224), interpolation=cv2.INTER_NEAREST)
-        image = np.expand_dims(image, axis=0)
-        image = image.astype(dtype=np.float64, copy=False)
-        image = image / 255.0
-
-        #TODO implement this
-        #pred = self.get_output([image, 0])[0]
-        #pred = np.argmax(pred)
-
-        # determine light color based on what is returned by model
-        # assuming model returns a number  0 == green, 1 == unknown, 2 == red, 3 == yellow
 
         return TrafficLight.UNKNOWN

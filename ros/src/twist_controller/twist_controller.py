bf30ac7429806bddb6573c8e7a816166e0ebcf1d
Bachmann, Michael <mbachm87@gmail.com>
2017-12-13 23:02:18 +0100

    Feature: Added calulation of brake value in twist_controller.py. Additionally added some logger for debugging in dbw_node.py

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -17,13 +17,15 @@ class Controller(object):
         self.max_steer_angle = kwargs['max_steer_angle']
         self.decel_limit = kwargs['decel_limit']
         self.accel_limit = kwargs['accel_limit']
-        self.pid_controller = PID(Kp, Ki, Kd, mn = self.decel_limit, mx = self.accel_limit)
+        self.wheel_radius = kwargs['wheel_radius']
+        self.pid_controller = PID(Kp, Ki, Kd, mn=self.decel_limit, mx=self.accel_limit)
         self.yaw_controller = YawController(self.wheel_base,
                                             self.steer_ratio,
                                             self.min_speed,
                                             self.max_lat_accel,
                                             self.max_steer_angle)
-        
+        self.vehicle_mass = kwargs['vehicle_mass'] + kwargs['fuel_capacity'] * GAS_DENSITY
+
 
     def control(self, current_velocity, twist_cmd, dbw_enabled, sample_time):
         # TODO: Change the arg, kwarg list to suit your needs
@@ -37,9 +39,16 @@ class Controller(object):
 
         linear_velocity_error = twist_cmd.twist.linear.x - current_velocity.twist.linear.x
 
-
         throttle = self.pid_controller.step(linear_velocity_error, sample_time)
-        brake = 0.0
+
+        throttle = max(0.0, min(1.0, throttle))
+        if linear_velocity_error < 0:
+            # brake value = weight of vehicle * deceleration (e.g. 4 m/s/s) * wheel has a radius
+            brake = self.vehicle_mass * -linear_velocity_error/sample_time * self.wheel_radius
+            throttle = 0.0
+        else:
+            brake = 0.0
+
         steering = self.yaw_controller.get_steering(twist_cmd.twist.linear.x,
                                                     twist_cmd.twist.angular.z,
                                                     current_velocity.twist.linear.x)
